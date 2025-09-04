8a733770f7ce16d14fa69c32b59caee2157b9adc
juniorSarh <sabelohgumede@gmail.com>
2025-09-04 08:55:01 +0200

    feature: working on footer and media queries

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,44 @@ export default function Footer() {
   return (
     
       <div className="footer">
-        <h1>Footer</h1>
+        <div className="navbar-logo">
+        <img src="src/assets/Images/logo.svg"></img>
+      </div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column'
+      }}>
+        <h4>Links</h4>
+        <a href="#home" style={{ margin: '4px 0' }}>Home</a>
+        <a href="#about" style={{ margin: '4px 0' }}>About</a>
+        <a href="#services" style={{ margin: '4px 0' }}>Services</a>
+        <a href="#contact" style={{ margin: '4px 0' }}>Contact</a>
+      </div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        margin:'30px',
+      }}>
+        <h4>List1</h4>
+        <ul style={{ paddingLeft: '20px', margin: 0
+          
+         }}>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column'
+      }}>
+        <h4>List2</h4>
+        <ul style={{ paddingLeft: '20px', margin: 0 }}>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </div>
       </div>
   );
 }
